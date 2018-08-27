8a863a7ed57fb44f615f51ca7242663c1b82e340
Miroslav Muras <mirekmuras@gmail.com>
2018-08-27 15:23:16 -0400

    initial commit

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, {Component} from 'react';
 
 //@dev: declare a new component Search Bar with an Input
 /*const SearchBar = () => {
@@ -6,12 +6,30 @@ import React, { Component} from 'react';
 };
 */
 
-//@dev: Class using React.Component funcinality
+//@dev: Class user event using React.Component funcinality
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {term: 'Starting Value'};                                                        //initial input value is EMPTY
+    }
+
+
     render() {
-        return <input onChange = {event => console.log(event.target.value)} /> ;
+    return (
+        <div>
+            <input 
+            value = {this.state.term}                                                   //controled input component set up by State
+            onChange = {event => this.setState({term: event.target.value})} />
+        </div>
+      );
     }
 }
 
+//@dev: What STATE is ?, plain javaScript object, exist ONLY on each 'class ' component have their own state,
+// record and react user event, whenever component is change it is imidiatly rendered
+
+
 //@dev: export the component
 export default SearchBar;
+
